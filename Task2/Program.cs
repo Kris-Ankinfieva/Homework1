530dec38c8721678ebf29be9710d6bc356ef2e35
Kris Ankinfieva <kk397758@yandex.ru>
2022-10-04 16:50:06 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine ("Веведите первое число: ");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine ("Веведите второе число: ");
+int b = int.Parse (Console.ReadLine());
+Console.WriteLine ("Веведите третье число: ");
+int c = int.Parse (Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine ($"Число {a} является максимальным");
+
+}
+else if (b > c)
+{
+    Console.WriteLine ($"Число {b} является максимальным");
+}
+else if (c > a)
+{
+    Console.WriteLine ($"Число {c}является максимальным");
+}
