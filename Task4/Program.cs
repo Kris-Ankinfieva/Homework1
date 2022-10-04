24d67d5c8f99aaea1d2768a4a1c476221baefcd5
Kris Ankinfieva <kk397758@yandex.ru>
2022-10-05 01:38:09 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("vvedite chislo: ");
+int a = int.Parse (Console.ReadLine());
+int max = 2;
+while (max <= a)
+{
+    Console.Write( max );
+    max = max + 2;
+}
